6b281200b53a745327d5b44ad020d33687644de7
J <j@MacBook-Pro-7.local>
2023-04-16 21:33:50 -0500

    added main activity and started appetizer activity

--- app/src/main/java/io/kwebdev/androidfoodapp/MainActivity.java
+++ app/src/main/java/io/kwebdev/androidfoodapp/MainActivity.java
@@ -1,14 +1,32 @@
 package io.kwebdev.androidfoodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView startersCard;
+    CardView mainsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // any time you want to move to a new screen in Android, it must be done with intents
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
